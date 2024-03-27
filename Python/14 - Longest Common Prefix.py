726f331a6203ec500ad6adda2b192470fd9597d2
Hadi k p <149652697+Haadikp@users.noreply.github.com>
2024-03-27 12:23:46 +0530

    Easy

---
+++ Python/14 - Longest Common Prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        compare = strs[0]
+        length = len(compare)
+        for i in strs[1:len(strs)]:
+            while length != 0:
+                if compare[0:length] == i[0:length]:
+                    compare = compare[0:length]
+                    break
+                else:
+                    length -= 1
+                    
+        return compare[0:length]
